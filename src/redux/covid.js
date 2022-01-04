bb3edafcd674b774d7a69d80ba794d14ec5a4cd8
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-04 22:26:28 +0200

    Add fetch reducer for covid data

---
+++ src/redux/covid.js
@@ -0,0 +1,30 @@
+const GET_COVID_DATA = 'GET_COVID_DATA'
+const CLEAR_DATA = "CLEAR_DATA"
+
+const initialState = {};
+
+const covidReducer = (state = initialState, action)=>{
+  switch (action.payload) {
+    case GET_COVID_DATA: 
+      return {
+        ...state, [action.payload.id] : action.payload
+      }
+    case CLEAR_DATA: return {}
+    default: return state;
+  }
+}
+
+export const getCovidData = (payload) => ({
+  type: GET_COVID_DATA,
+  payload
+});
+
+export const clearCovidData = () => ({
+  type: CLEAR_DATA
+});
+
+export const fetchCovidData = (rpl) => async (dispatch) => {
+  const date = new Date();
+  const dateString = date.toISOString().slice(0, 10)
+
+}
