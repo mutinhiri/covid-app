522aa0cabf4fcf95366c123bc7d17d45af074591
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-05 19:18:53 +0200

    Add Information component and basic setup

---
+++ src/components/Information.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import PropTypes from 'prop-types'
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { MdArrowBackIosNew } from 'react-icons/md'
+import { fetchCovidData } from "../redux/covid";
+
+export const Information = (props) => {
+  const
+  return (
+    <div>
+      
+    </div>
+  )
+}
