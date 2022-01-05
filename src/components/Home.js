6b7fdc196c24856a06b1eba8533acfb79b5f30e3
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-05 17:59:30 +0200

    Add onChange function to the component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux'
 import fetchAllData from '../redux/data'
+import Map from '../us-map.png'
 
 export const Home = () => {
   const states = useSelector((state) => state.data);
@@ -17,8 +18,19 @@ export const Home = () => {
       <div className='header'>
         All states
       </div>
-      
-      
+      <div className='main-card'>
+        <img src={Map} alt="map-for-state" />
+        <div>
+          <h1>United States</h1>
+        </div>
+      </div>
+      <input
+        type="text"
+        className='search-bar'
+        placeholder='Search here'
+        onChange={(e) => {
+          setSearchState(e.target.value);
+      } }/>
     </div>
   )
 }
