f26f4945c76796cecf2ed368c3a777a903253391
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-05 16:56:36 +0200

    Add fetch data to covid reducer

--- src/redux/covid.js
+++ src/redux/covid.js
@@ -38,7 +38,16 @@ export const fetchCovidData = (rpl) => async (dispatch) => {
   dispatch(
     getCovidData({
       id: covidData.id,
-      confirmed: 
+      confirmed: covidData.today_new_confirmed,
+      deaths: covidData.today_deaths,
+      new_deaths: covidData.today_new_deaths,
+      open_cases: covidData.today_open_cases,
+      new_open_cases: covidData.today_new_open_cases,
+      recovered: covidData.today_recovered,
+      new_recovered: covidData.today_new_recovered,
+      tests: covidData.today_tests,
+      new_tests: covidData.today_new_tests,
+      total_hospitalised_patients: covidData.today_new_total_hospitalised_patients,
     })
   )
 
