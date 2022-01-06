5b828cbb47a1835757065ccdd3b81e74d36c94d9
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-06 19:40:46 +0200

    Add App test final

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import renderer from 'react-test-renderer'
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-storage'
-import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store'
+import App from '../App';
+import data from './__mock__/data_mock.json'
+
+describe('Application test', () => {
+  const initialState = { data };
+  const mockStore = configureStore();
+  let store;
+
+  it('Renders correctly', () => {
+    store = mockStore(initialState)
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <App/>
+        </Provider>
+    )
+  })
+})
