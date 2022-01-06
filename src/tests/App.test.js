07780bb14b675ddfac4b6aadb0553911968b7685
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-06 19:31:15 +0200

    Add test render and redux mock store

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import '@testing-library/jest-dom';
+import renderer from 'react-test-renderer'
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-storage'
 import App from '../App';
 
 test('renders learn react link', () => {
